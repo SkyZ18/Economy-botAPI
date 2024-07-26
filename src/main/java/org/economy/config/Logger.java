aeead7402e7d49ea2723641ec70fbe0ba048eed8
SkyZ18 <114467762+SkyZ18@users.noreply.github.com>
2024-07-26 14:24:47 +0200

    Refactor

--- src/main/java/org/economy/config/Logger.java
+++ src/main/java/org/economy/config/Logger.java
@@ -12,7 +12,7 @@ public class Logger {
     public static void log(String message, LogType logType) {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String formattedMessage = String.format("{%s} {%s} %s", now.format(formatter), logType, message);
+        String formattedMessage = String.format("[%s] [%s] %s", now.format(formatter), logType, message);
         System.out.println(formattedMessage);
     }
 
